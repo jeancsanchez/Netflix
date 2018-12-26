f49e8dfea3dcb30e4733bf779e0fbbb512c04fdb
jean <jeancs.patricio@gmail.com>
2018-12-26 15:05:10 -0300

    Criando modelos.

--- core/netflix/admin.py
+++ core/netflix/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from netflix.models import Filme
+
+admin.site.register(Filme)
--- core/netflix/models.py
+++ core/netflix/models.py
@@ -1,3 +1,57 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = 'Gêneros'
+
+
+class Ator(models.Model):
+    sexos = (
+        (1, 'M'),
+        (2, 'F')
+    )
+
+    nome = models.CharField(max_length=100)
+    sexo = models.IntegerField(choices=sexos)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = 'Atores'
+
+
+class Idioma(models.Model):
+    nome = models.CharField(max_length=100)
+    sigla = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.sigla
+
+    class Meta:
+        verbose_name_plural = 'Idiomas'
+
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    resumo = models.TextField(max_length=100)
+    genero = models.ForeignKey(Genero, on_delete=models.PROTECT)
+    duracao = models.DurationField(default=0)
+    elenco = models.ManyToManyField(Ator)
+    link = models.URLField()
+    estrelas = models.IntegerField(default=0)
+    idioma = models.ForeignKey(Idioma, on_delete=models.PROTECT)
+    lancamento = models.DateField()
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name_plural = 'Filmes'
