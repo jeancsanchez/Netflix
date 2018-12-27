6a2c56ff3b698d2396d0fb9e40cdaeed7e679e8b
jean <jeancs.patricio@gmail.com>
2018-12-27 15:37:26 -0300

    Personalizando admin do Filme.

--- core/netflix/admin.py
+++ core/netflix/admin.py
@@ -4,13 +4,19 @@ from django.contrib.admin import register
 from netflix.models import *
 
 
+class ElencoInline(admin.TabularInline):
+    model = Filme.elenco.through
+    verbose_name_plural = 'Elenco'
+
+
 @register(Filme)
 class FilmeAdmin(admin.ModelAdmin):
+    inlines = [ElencoInline, ]
     list_display = ('titulo', 'resumo', 'idioma')
     list_filter = ('lancamento', 'genero', 'idioma', 'estrelas')
+    exclude = ('elenco',)
 
 
 admin.site.register(Idioma)
-admin.site.register(Ator)
 admin.site.register(Produtora)
 admin.site.register(Genero)
